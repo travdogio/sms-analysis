4edb8be6548c6d142568abb2fae159ed2c8ffc30
TravdogIO <metaperspective@gmail.com>
2020-07-19 18:17:10 -0700

    Export to CSV
    Export a list of contacts and number of messages each

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import smsutils.xmlcleaner as cleaner
 import smsutils.stats as stats
+import csv
 
 # Removes the MMS entries from the log
 # cleaner.remove_non_sms('data/sms.xml', 'data/newoutput.xml')
 
 myStats = stats.Stats('data/clean.xml')
-print(myStats.message_count(''))
+contacts_dict = myStats.contact_stats()
+with open('data/contact_stats.csv', 'w', encoding='utf8', newline='') as f:
+    w = csv.writer(f)
+    for contact in contacts_dict:
+        w.writerow([contact, contacts_dict[contact]])
+
+
+
+
+# for contact in c_dict:
+#     print(contact + " : " + str(c_dict[contact]))
--- smsutils/stats.py
+++ smsutils/stats.py
@@ -1,10 +1,20 @@
 import xml.etree.ElementTree as ET
+import csv
 
 
 class Stats:
     def __init__(self, input_filename):
         self.tree = ET.parse(input_filename)
         self.root = self.tree.getroot()
+
+    def contact_stats(self):
+        contacts = {}
+        for element in self.root:
+            if element.get('contact_name') not in contacts:
+                contacts[element.get('contact_name')] = 1
+            else:
+                contacts[element.get('contact_name')] += 1
+        return contacts
 
     def message_count(self, name):
         total = 0
